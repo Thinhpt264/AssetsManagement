4a3af8cdfc0f291f11e9708796703fbe5986b761
tranquangvu2003 <144189279+tranquangvu2003@users.noreply.github.com>
2025-03-27 17:58:55 +0700

    update api

--- src/main/java/com/example/demo/controller/AssetController.java
+++ src/main/java/com/example/demo/controller/AssetController.java
@@ -39,27 +39,5 @@ public class AssetController {
         return new ResponseEntity<>("Asset deleted successfully", HttpStatus.OK);
     }
 
-    @PostMapping("/createAsset")
-    public ResponseEntity<String> createAsset(@RequestBody Asset asset){
-        try {
-            String result = service.createAsset(asset);
-            if(result.equals("Asset saved successfully")){
-                return new ResponseEntity<>(result, HttpStatus.CREATED);
-            }else{
-                return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
-            }
-        }catch (Exception e){
-            return new ResponseEntity<>("register failed: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-    @GetMapping("/statistics")
-    public ResponseEntity<?> getAssetStatisticsByStatus() {
-        List<AssetStatusStatsDTO> assetStatsList = service.getAssetStatisticsByStatus();
-        if (assetStatsList == null || assetStatsList.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .body("No asset statistics found");
-        }
-        return ResponseEntity.ok(assetStatsList);
-    }
 
 }
